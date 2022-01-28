58cbb590568f582ed492e1bf919774329e57fc7e
Océane Raubitschek <oceane>
2022-01-28 17:36:35 +0100

    add login form

--- backSportsAdmin/config/bundles.php
+++ backSportsAdmin/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
