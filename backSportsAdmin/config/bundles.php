0e2556b9958fdec2545a0cfd3715707c410c6fb7
Océane Raubitschek <oceane>
2022-02-11 11:11:40 +0100

    remove database

--- backSportsAdmin/config/bundles.php
+++ backSportsAdmin/config/bundles.php
@@ -14,4 +14,7 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    
+
+
 ];
