7cf1c50688e51451e6a2b539ec77081e16088c65
Nathalie Lavroff <nathalielavroff@gmail.com>
2022-02-14 13:35:50 +0100

    added route and nav to template

---
+++ frontSportsAdmin/src/app/page/club/main-content/list-members/list-members.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-members',
+  templateUrl: './list-members.component.html',
+  styleUrls: ['./list-members.component.scss']
+})
+export class ListMembersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontSportsAdmin/src/app/page/club/view/list-members-view/list-members-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-members-view',
+  templateUrl: './list-members-view.component.html',
+  styleUrls: ['./list-members-view.component.scss']
+})
+export class ListMembersViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
