84b86ba3bf7cec889dcc5ff3be1d8cff3377a204
Océane Raubitschek <oceane>
2022-02-15 13:58:59 +0100

    create a new header-bar for mes clubs, btn colorés en dur + side bar btn coloré en dur

--- frontSportsAdmin/src/app/page/club/main-content/dashboard/dashboard.component.ts
+++ frontSportsAdmin/src/app/page/club/main-content/dashboard/dashboard.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { EditClubComponent } from '../edit-club/edit-club.component';
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
