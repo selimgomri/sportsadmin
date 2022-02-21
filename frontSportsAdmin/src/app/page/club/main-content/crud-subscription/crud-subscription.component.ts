ce8248103fab6036b699a8c5bd44dea90f225028
Selim <selimgomri@gmail.com>
2022-02-21 14:58:31 +0100

    generated componenet crud subscription

---
+++ frontSportsAdmin/src/app/page/club/main-content/crud-subscription/crud-subscription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'crud-subscription',
+  templateUrl: './crud-subscription.component.html',
+  styleUrls: ['./crud-subscription.component.scss']
+})
+export class CRUDSubscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontSportsAdmin/src/app/page/club/view/crud-subscription-view/crud-subscription-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'crud-subscription-view',
+  templateUrl: './crud-subscription-view.component.html',
+  styleUrls: ['./crud-subscription-view.component.scss']
+})
+export class CRUDSubscriptionViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
