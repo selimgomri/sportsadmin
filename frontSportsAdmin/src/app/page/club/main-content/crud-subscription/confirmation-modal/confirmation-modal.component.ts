91bf7932a85910a39dfbabdb3b36efbbc5ffd340
Selim <selimgomri@gmail.com>
2022-02-22 16:45:07 +0100

    trash is displayed

--- frontSportsAdmin/src/app/page/club/main-content/crud-subscription/confirmation-modal/confirmation-modal.component.ts
+++ frontSportsAdmin/src/app/page/club/main-content/crud-subscription/confirmation-modal/confirmation-modal.component.ts
@@ -2,7 +2,7 @@ import { Component, Type } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'confirmation-modal',
+  selector: 'delete-modal',
   template: `
     <div class="modal-header">
       <h4 class="modal-title" id="modal-title">Profile deletion</h4>
@@ -44,69 +44,20 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
     </div>
   `,
 })
-export class ConfirmationModalComponent {
+export class DeleteModalComponent {
   constructor(public modal: NgbActiveModal) {}
 }
 
-@Component({
-  selector: 'ngbd-modal-confirm-autofocus',
-  template: `
-    <div class="modal-header">
-      <h4 class="modal-title" id="modal-title">Profile deletion</h4>
-      <button
-        type="button"
-        class="btn-close"
-        aria-label="Close button"
-        aria-describedby="modal-title"
-        (click)="modal.dismiss('Cross click')"
-      ></button>
-    </div>
-    <div class="modal-body">
-      <p>
-        <strong
-          >Are you sure you want to delete
-          <span class="text-primary">"John Doe"</span> profile?</strong
-        >
-      </p>
-      <p>
-        All information associated to this user profile will be permanently
-        deleted.
-        <span class="text-danger">This operation can not be undone.</span>
-      </p>
-    </div>
-    <div class="modal-footer">
-      <button
-        type="button"
-        class="btn btn-outline-secondary"
-        (click)="modal.dismiss('cancel click')"
-      >
-        Cancel
-      </button>
-      <button
-        type="button"
-        ngbAutofocus
-        class="btn btn-danger"
-        (click)="modal.close('Ok click')"
-      >
-        Ok
-      </button>
-    </div>
-  `,
-})
-export class NgbdModalConfirmAutofocus {
-  constructor(public modal: NgbActiveModal) {}
-}
 
 const MODALS: { [name: string]: Type<any> } = {
-  focusFirst: ConfirmationModalComponent,
-  autofocus: NgbdModalConfirmAutofocus,
+  focusFirst: DeleteModalComponent,
 };
 
 @Component({
-  selector: 'ngbd-modal-focus',
+  selector: 'confirmation-modal',
   templateUrl: './confirmation-modal.component.html',
 })
-export class NgbdModalFocus {
+export class ConfirmationModalComponent {
   withAutofocus = `<button type="button" ngbAutofocus class="btn btn-danger"
       (click)="modal.close('Ok click')">Ok</button>`;
 
