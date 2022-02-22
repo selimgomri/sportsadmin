83bb100bd35fe2c409270e02c84f71674f24eed9
Selim <selimgomri@gmail.com>
2022-02-22 16:58:25 +0100

    modal works

--- frontSportsAdmin/src/app/page/club/main-content/crud-subscription/confirmation-modal/confirmation-modal.component.ts
+++ frontSportsAdmin/src/app/page/club/main-content/crud-subscription/confirmation-modal/confirmation-modal.component.ts
@@ -48,9 +48,32 @@ export class DeleteModalComponent {
   constructor(public modal: NgbActiveModal) {}
 }
 
+@Component({
+  selector: 'ngbd-modal-confirm-autofocus',
+  template: `
+  <div class="modal-header">
+    <h4 class="modal-title" id="modal-title">Profile deletion</h4>
+    <button type="button" class="btn-close" aria-label="Close button" aria-describedby="modal-title" (click)="modal.dismiss('Cross click')"></button>
+  </div>
+  <div class="modal-body">
+    <p><strong>Are you sure you want to delete <span class="text-primary">"John Doe"</span> profile?</strong></p>
+    <p>All information associated to this user profile will be permanently deleted.
+    <span class="text-danger">This operation can not be undone.</span>
+    </p>
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-outline-secondary" (click)="modal.dismiss('cancel click')">Cancel</button>
+    <button type="button" ngbAutofocus class="btn btn-danger" (click)="modal.close('Ok click')">Ok</button>
+  </div>
+  `
+})
+export class NgbdModalConfirmAutofocus {
+  constructor(public modal: NgbActiveModal) {}
+}
 
 const MODALS: { [name: string]: Type<any> } = {
   focusFirst: DeleteModalComponent,
+  autofocus: NgbdModalConfirmAutofocus
 };
 
 @Component({
