592b4c39c32e3263d3b600a031dc351c74ad2103
Selim <selimgomri@gmail.com>
2022-02-22 14:15:40 +0100

    added a margin to subscriptions

--- frontSportsAdmin/src/app/page/club/main-content/crud-subscription/crud-subscription.component.ts
+++ frontSportsAdmin/src/app/page/club/main-content/crud-subscription/crud-subscription.component.ts
@@ -1,4 +1,3 @@
-import { Router } from '@angular/router';
 import { ISubscription } from './../../../../services/subscription/ISubscription';
 import { SubscriptionService } from './../../../../services/subscription/subscription.service';
 import { Component, OnInit } from '@angular/core';
@@ -13,30 +12,23 @@ export class CRUDSubscriptionComponent implements OnInit {
 
   constructor(
     private subscriptionService: SubscriptionService,
-    private route: Router
   ) {}
 
   ngOnInit(): void {
     this.subscriptionService.getSubscriptions().subscribe((datas: any) => {
       this.subscriptions = datas['hydra:member'];
-      console.log(this.subscriptions);
     });
   }
 
   onRemoveClick(subscription: any) {
     if (!subscription) return;
-    //console.log(subscription);
     this.subscriptionService
       .deleteSubscription(subscription.id)
       .subscribe(() => {
         if (!subscription) return;
         this.subscriptions = this.subscriptions.filter((deleted) => {
-          console.log(deleted.id);
-          console.log('separation');
           return deleted.id !== subscription.id;
-        }); //subscription.id != deleted);
-        //console.log(this.subscriptions);
-        //this.route.navigate(['/gestion-des-cotisations']);
+        });
       });
   }
 }
