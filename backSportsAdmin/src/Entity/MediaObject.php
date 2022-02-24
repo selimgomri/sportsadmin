e3dc5b50611eee32338603b51b96827365fd0e6c
Océane Raubitschek <oceane>
2022-02-24 15:01:41 +0100

    add upload function

---
+++ backSportsAdmin/src/Entity/MediaObject.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MediaObjectRepository;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Controller\Api\CreateMediaObjectAction;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @Vich\Uploadable
+ */
+#[ORM\Entity(repositoryClass: MediaObjectRepository::class)]
+#[ApiResource(
+    iri: 'http://schema.org/MediaObject',
+    normalizationContext: ['groups' => ['media_object:read']],
+    itemOperations: ['get'],
+    collectionOperations: [
+        'get',
+        'post' => [
+            'controller' => CreateMediaObjectAction::class,
+            'deserialize' => false,
+            'validation_groups' => ['Default', 'media_object_create'],
+            'openapi_context' => [
+                'requestBody' => [
+                    'content' => [
+                        'multipart/form-data' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'file' => [
+                                        'type' => 'string',
+                                        'format' => 'binary',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ]
+)]
+class MediaObject
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    #[Groups(['media_object:read'])]
+    private $id;
+
+    #[ApiProperty(iri: 'http://schema.org/contentUrl')]
+    #[Groups(['media_object:read'])]
+    public ?string $contentUrl = null;
+    
+    /**
+     * @Vich\UploadableField(mapping="media_object", fileNameProperty="filePath")
+     */
+    #[Assert\NotNull(groups: ['media_object_create'])]
+    public ?File $file = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $filePath;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFilePath(): ?string
+    {
+        return $this->filePath;
+    }
+
+    public function setFilePath(?string $filePath): self
+    {
+        $this->filePath = $filePath;
+
+        return $this;
+    }
+}
