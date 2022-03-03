dc61bed7ee76346b6222ed3739dcd77fe721f617
smectaz <90684643+smectaz@users.noreply.github.com>
2022-03-03 17:21:25 +0100

    test recup email

--- frontSportsAdmin/src/app/oubli-password/oubli-password.component.ts
+++ frontSportsAdmin/src/app/oubli-password/oubli-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/session-login/api.service';
+import { IUser } from 'src/app/IUser';
 
 @Component({
   selector: 'app-oubli-password',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OubliPasswordComponent implements OnInit {
 
-  constructor() { }
+  users: IUser[] = [];
+  sortedUsers = this.users;
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.apiService.getUsers().subscribe((datas: any) => {
+      this.users = datas['hydra:member'];
+      this.sortedUsers = this.users;
+    });
   }
 
 }
