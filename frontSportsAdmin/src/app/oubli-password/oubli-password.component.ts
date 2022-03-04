53a3e06bd77be32cd71a7bccbe89ab0a274cc078
smectaz <90684643+smectaz@users.noreply.github.com>
2022-03-04 02:13:55 +0100

    create form de reinitialisation password

--- frontSportsAdmin/src/app/oubli-password/oubli-password.component.ts
+++ frontSportsAdmin/src/app/oubli-password/oubli-password.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/session-login/api.service';
 import { IUser } from 'src/app/IUser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SessionLoginService } from 'src/app/services/session-login/session-login.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-oubli-password',
@@ -10,15 +13,47 @@ import { IUser } from 'src/app/IUser';
 export class OubliPasswordComponent implements OnInit {
 
   users: IUser[] = [];
+  user?: IUser;
+  id!: number;
   sortedUsers = this.users;
+  form!: FormGroup;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private sessionLoginService: SessionLoginService, private apiService: ApiService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.sessionLoginService.userProfile.subscribe((data) => {
+      this.user = data;
+      console.log(this.user);
+    });
+
+    this.id = this.route.snapshot.params['postId'];
+    this.apiService.find(this.id).subscribe((data: IUser)=>{
+      this.user = data;
+    });
+
     this.apiService.getUsers().subscribe((datas: any) => {
       this.users = datas['hydra:member'];
       this.sortedUsers = this.users;
     });
+
+    this.form = new FormGroup({
+
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+
+
+    });
   }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.apiService.updateProfile(this.id, this.form.value).subscribe((res:any) => {
+         console.log('Post updated successfully!');
+        })
+      }
 
 }
