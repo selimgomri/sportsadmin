650c23f505578e338c2586c68fffcf728e324173
Nathalie Lavroff <nathalielavroff@gmail.com>
2022-03-08 16:28:40 +0100

    added modal for delete

--- frontSportsAdmin/src/app/listing/listing.component.ts
+++ frontSportsAdmin/src/app/listing/listing.component.ts
@@ -123,6 +123,14 @@ export class ListingComponent {
     });
   }
 
+  open(content:any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
