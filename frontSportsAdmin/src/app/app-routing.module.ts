21a4a0cb600fbd89d341a08c97823304fd156a3e
smectaz <smectaz57@gmail.com>
2022-03-11 10:15:17 +0100

    fix angular

--- frontSportsAdmin/src/app/app-routing.module.ts
+++ frontSportsAdmin/src/app/app-routing.module.ts
@@ -11,7 +11,6 @@ import { AuthGuard } from './shared/auth.guard';
 import { ProfileComponent } from './page/club/main-content/profile/profile.component';
 import { ProfileViewComponent } from './page/club/view/profile-view/profile-view.component';
 import { AddUserComponent } from './page/club/main-content/edit-club/crud-user/add-user/add-user.component';
-import { OubliPasswordComponent } from './oubli-password/oubli-password.component';
 import { CreateUserViewComponent } from './page/club/view/create-user-view/create-user-view.component';
 
 const routes: Routes = [
@@ -89,16 +88,14 @@ const routes: Routes = [
     path: 'ajouter-membre',
     component: CreateUserViewComponent,
   },
-  {
-    path: 'oubli-password',
-    component: OubliPasswordComponent,
-  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
-})],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabledBlocking',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
