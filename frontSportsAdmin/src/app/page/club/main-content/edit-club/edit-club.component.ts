4a29e8a5582eb4c07e9fa9572c12ad356a7bd853
Nathalie Lavroff <nathalielavroff@gmail.com>
2022-03-15 10:06:45 +0100

    default profile picture added in listing

--- frontSportsAdmin/src/app/page/club/main-content/edit-club/edit-club.component.ts
+++ frontSportsAdmin/src/app/page/club/main-content/edit-club/edit-club.component.ts
@@ -22,9 +22,7 @@ export class EditClubComponent implements OnInit {
 
   ngOnInit(): void {
     this.clubService.getClub(5).subscribe((datas: any) => {
-      console.log(datas);
       this.club = datas;
-      console.log(this.club);
       //this.id = this.club.id;
     });
 
