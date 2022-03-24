05c2b0b41a674febed30e4fce2b335143bb82f90
Nathalie Lavroff <nathalielavroff@gmail.com>
2022-03-24 10:12:42 +0100

    added dynamic infos in dashboard

--- frontSportsAdmin/src/app/page/club/main-content/list-members/list-members.component.ts
+++ frontSportsAdmin/src/app/page/club/main-content/list-members/list-members.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input, OnInit} from '@angular/core';
-import { IUser } from 'src/app/IUser';
-import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-list-members',
@@ -11,13 +9,20 @@ import { UsersService } from 'src/app/services/users.service';
 export class ListMembersComponent implements OnInit {
 
   usersNumber!: number;
-  licensedUsers!: IUser[];
-  unlicensedUsers!: IUser[];
-  constructor(private apiService: UsersService) {}
+  licensedNumber!: number;
+  unlicensedNumber!: number;
+
+  constructor() {}
 
   ngOnInit(): void {}
 
   getUsersLength(users:number) {
     this.usersNumber = users;
+  }
+  getLicensedLength(users:number) {
+    this.licensedNumber = users;
+  }
+  getUnlicensedLength(users:number) {
+    this.unlicensedNumber = users;
   }
 }
